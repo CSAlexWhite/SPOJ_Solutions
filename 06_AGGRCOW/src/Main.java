8f6e012afd25a952cb5d76d6dcb5a326db827292
Alex White <alexwhite123@gmail.com>
2016-04-22 11:12:22 -0400

    Works (I think) but took too long on SPOJ

--- 06_AGGRCOW/src/Main.java
+++ 06_AGGRCOW/src/Main.java
@@ -1,5 +1,6 @@
 /**
  * started April 21, 2016
+ * finished April 22, 2016
  * from http://www.spoj.com/problems/GNYRO9F
  *
  * Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a
@@ -14,66 +15,52 @@ import java.lang.*;
 
 class Main {
 
-    static int maxiMin;
     static ArrayList<Integer> emptyStalls;
 
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc = new Scanner(System.in);
-        int tests = sc.nextInt();
 
-        int stalls, cows;
+        int stalls, cows, tests = sc.nextInt();
         for(int i=0; i<tests; i++){
 
             emptyStalls = new ArrayList<>();
-            stalls = sc.nextInt();
-            cows = sc.nextInt();
+            stalls = sc.nextInt(); cows = sc.nextInt();
 
-            for(int j=0; j<stalls; j++) {
+            for(int j=0; j<stalls; j++)
+                emptyStalls.add(sc.nextInt());
 
-                int next = sc.nextInt();
-                emptyStalls.add(next);
-            }
+            emptyStalls.sort((Integer o1, Integer o2) -> o1-o2);
 
-//            System.out.println("Test " + i);
-//            System.out.println(stalls + " stalls, " + cows + " cows");
-//            System.out.print("Stalls at: ");
-//            for(Integer elt : emptyStalls) System.out.print(elt + " ");
-
-            emptyStalls.sort(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o1-o2;
-                }
-            });
-
-            //for(Integer elt : emptyStalls) System.out.print(elt + " ");
-
-            //System.out.println("\n" + "Max Min is " + getMaxMin(emptyStalls, cows) + "\n");
             System.out.println(getMaxMin(emptyStalls, cows));
         }
     }
 
+
+    /**
+     *  Algorithm does a binary search for the max minimum distance between stalls
+     * (1) define the bounds of the search as the first and last stall
+     * (2) choose a midpoint
+     * (3) evaluate the stalls greedily (i.e. choose the first stall, then add the
+     * midpoint value until either (a) no more cows are left or (b) all the stalls are
+     * filled and there are cows left).
+     * (4) reevaluate the bounds and search again
+     *
+     * @param stalls
+     * @param numCows
+     * @return
+     */
     public static int getMaxMin(ArrayList<Integer> stalls, int numCows){
 
-        int min, max, answer = 0;
+        int hi, lo, min, max, target, nextVal, answer = 0;          // min and max are stall bounds
+        int[] stallAssignment = new int[numCows];                   // lo and hi are search bounds
 
-        int lo = min = stalls.get(0);
-        int hi = max = stalls.get(stalls.size()-1);
-        int[] stallAssignment = new int[numCows];
+        lo = min = stalls.get(0);
+        hi = max = stalls.get(stalls.size()-1);
 
-        /**
-         * Algorithm does a binary search for the max minimum.
-         * (1) define the bounds of the search as the first and last stall
-         * (2) choose a midpoint
-         * (3) evaluate the stalls greedily (i.e. choose the first stall, then add the
-         * midpoint value until either (a) no more cows are left or (b) all the stalls are
-         * filled and there are cows left).
-         * (4) reevaluate the bounds and search again
-         */
         while(lo < hi){
 
-            int target = lo + (hi-lo)/2;
+            target = lo + (hi-lo)/2;        // pick the midpoint of the array
 
             boolean success = false;
             Arrays.fill(stallAssignment, 0);
@@ -81,40 +68,39 @@ class Main {
 
             for(int i=1; i<numCows; i++){
 
-                if((stallAssignment[i-1] + target) > max){
+                if((stallAssignment[i-1] + target) > max) break;    // if there can't be more stalls
 
-//                    System.out.println("IMPOSSIBLE: Next should be above " + (stallAssignment[i-1] + target) + " and below " + max);
-                    break;
-                }
+                nextVal = findNextHigher(stalls, stallAssignment[i-1], target);
 
-                int valueToAssign = findNextHigher(stalls, stallAssignment[i-1], target); // next one higher...
+                if(nextVal == -1) break;                // if can't find any stalls higher than this
+                if(i == numCows-1) success = true;      // if placed all cows, search for a higher
 
-                if(valueToAssign == -1) break;
-                stallAssignment[i] = valueToAssign;
-                if(i == numCows-1) success = true;      // if placed all cows, search left
-
-//                System.out.print("Target distance is " + target + " : ");
-//                for(Integer elt : stallAssignment) System.out.print(elt + " ");
-//                System.out.println();
+                stallAssignment[i] = nextVal;
             }
 
-            if(success) {
-
-                answer = target;
-                lo = target + 1;
-            }
-            else hi = target;
+            if(success) { answer = target; lo = target + 1; }   // look move bounds higher
+            else hi = target;                                   // move bounds lower
         }
 
         return answer;
     }
 
-    public static int findNextHigher(ArrayList<Integer> elements, int current, int increment) throws IndexOutOfBoundsException{
+    /**
+     * Searches the list of stalls for the next one higher than the current one plus the target min distance.
+     *
+     * @param elements
+     * @param current
+     * @param increment
+     * @return
+     * @throws IndexOutOfBoundsException
+     */
+    public static int findNextHigher(ArrayList<Integer> elements, int current, int increment)
+            throws IndexOutOfBoundsException{
 
         int i = elements.indexOf(current);
 
         try{ while(elements.get(++i) < current + increment) continue;}
-        catch(IndexOutOfBoundsException e){return -1;}
+        catch(IndexOutOfBoundsException e) { return -1; }
 
         return elements.get(i);
     }
